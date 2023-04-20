fb611cd807c51cd3093feba2b66291acd7c22613
minkman <lomick2090@gmail.com>
2023-04-20 12:54:57 -0500

    Create initial setup

---
+++ pic-some/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+
+export default function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path='/'>
+                    <Route index />
+
+
+
+                </Route>
+            </Routes>
+        </div>
+    )
+}
